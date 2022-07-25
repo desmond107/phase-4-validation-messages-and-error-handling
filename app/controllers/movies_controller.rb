11a023fbfe3b75664bfd1452a3ba27514c83b2d9
desmond <desmond@desmonds-MacBook-Pro.local>
2022-07-25 11:21:27 +0300

    task complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,13 @@
 class MoviesController < ApplicationController
   wrap_parameters false
   
+  def create
+    movie = Movie.create!(movie_params)
+    render json: movie, status: :created
+  rescue ActiveRecord::RecordInvalid => e
+    render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
+  end
+  
   def index
     movies = Movie.all
     render json: movies
